92b974592b70aa42ab5fed91d98e9bb5e53b6252
EunYeongKim <povoq1024@gmail.com>
2020-10-21 15:37:33 +0900

    [EMO-2] 메모리 스토리지 * 메모리 스토리지 CRUD 구현

---
+++ EUNMO/EUNMO/Service/MemoStorageType.swift
@@ -0,0 +1,24 @@
+//
+//  MemoStorageType.swift
+//  EUNMO
+//
+//  Created by 60080252 on 2020/10/21.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+    
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
