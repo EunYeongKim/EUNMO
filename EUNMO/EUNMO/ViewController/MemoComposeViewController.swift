9f484641868774ccf60c6edc4e27fe4afc529df7
EunYeongKim <povoq1024@gmail.com>
2020-10-26 15:06:17 +0900

    [EMO-3] Scene, ViewController, ViewModel파일 추가 * Scene 추가 * ViewController파일 추가 * ViewModel파일 추가

---
+++ EUNMO/EUNMO/ViewController/MemoComposeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MemoComposeViewController.swift
+//  EUNMO
+//
+//  Created by 60080252 on 2020/10/26.
+//
+
+import UIKit
+
+class MemoComposeViewController: UIViewController, ViewModelBindableType {
+
+    var viewModel: MemoComposeViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func bindViewModel() {
+        
+    }
+}
---
+++ EUNMO/EUNMO/ViewController/MemoDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MemoDetailViewController.swift
+//  EUNMO
+//
+//  Created by 60080252 on 2020/10/26.
+//
+
+import UIKit
+
+class MemoDetailViewController: UIViewController, ViewModelBindableType {
+    
+    var viewModel: MemoDetailViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func bindViewModel() {
+        
+    }
+}
