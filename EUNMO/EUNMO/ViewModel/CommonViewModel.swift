429f5a4aff52082e66cf5efb2142e58c3f624f98
EunYeongKim <povoq1024@gmail.com>
2020-10-27 10:18:11 +0900

    [EMO-5]MemoListViewModel구현 * CommonViewModel : 공통적으로 사용하는 기능, 모든 View들이 NavigationController에 embed되어 있으므로 title설정과 SceneCoordinator, storage 설정 * MemoListViewModel 구현 * MemoListViewController 구현

---
+++ EUNMO/EUNMO/ViewModel/CommonViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CommonViewModel.swift
+//  EUNMO
+//
+//  Created by 60080252 on 2020/10/27.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CommonViewModel: NSObject {
+    let title: Driver<String>
+    let sceneCoordinator: SceneCoordinatorType
+    let storage: MemoStorageType
+    
+    init(title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.title = Observable.just(title).asDriver(onErrorJustReturn: "")
+        self.sceneCoordinator = sceneCoordinator
+        self.storage = storage
+    }
+}
